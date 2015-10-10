eb9ebfc33f701af20c2ab7fe1246d7006dc6a0dd
Martin Codrington <mjmcodrington@gmail.com>
2015-10-10 12:10:54 -0600

    I added three tests to make the purpose of Collection.next() clear

--- sandbox/src/sandbox/core/collection/implementations/RCollection.java
+++ sandbox/src/sandbox/core/collection/implementations/RCollection.java
@@ -10,17 +10,18 @@ public class RCollection<Type> implements Collection<Type>{
 		if(_array == null) throw new NullPointerException();
 		this.array = _array;
 		this.len = this.array.length;
+		
+		//TODO You need to keep a variable to keep track of looping over the contents of the collection
+		//See test testSumCollection() to see what I mean
 	}
 	
 		               
-	@Override public Type at(int index) throws IndexOutOfBoundsException{
-		
-		if(len == 0) throw new IndexOutOfBoundsException("The Collection is empty");
-		
-		if(index<0||index>=len) throw new IndexOutOfBoundsException("Index must be between "
-									+ "0 and " + len + "you supplied " + index);
+	@Override public Type at(int index) throws IndexOutOfBoundsException{	
+		if(len == 0) throw new IndexOutOfBoundsException("The Collection is empty");	
+		if(index<0||index>=len) throw new IndexOutOfBoundsException(
+				"Index must be between 0 and " + len + "you supplied " + index);
 		return array[index];
-		}
+	}
 
 	@Override
 	public Type first(){
@@ -39,6 +40,7 @@ public class RCollection<Type> implements Collection<Type>{
 
 	@Override
 	public boolean hasNext() {
+		//TODO Look at the test testNext1() to see how this should work
 		int j;
 		for(j=0; j<len-1; j++)
 		if (this.at(j) != null);
@@ -48,13 +50,15 @@ public class RCollection<Type> implements Collection<Type>{
 
 	@Override
 	public Type next() throws IndexOutOfBoundsException {
+		//TODO Next is supposed to be an easier way to loop over all the elements of the collection
+		//Look at test testSumCollection(), I explain in more detail there
 		return this.at(len);
 	}
 
 
 	@Override
 	public void reset() {
-		// TODO Auto-generated method stub
+		// TODO Reset should reset the looping to the beginning
 		
 	}
 
--- sandbox/src/tests/CollectionTests.java
+++ sandbox/src/tests/CollectionTests.java
@@ -51,19 +51,105 @@ public class CollectionTests {
 			
 		}
 		
-		@Test public void testHasNext(){
-			assertNotNull(sut.at(0));
-			assertNotNull(sut.at(1));
-			assertNotNull(sut.at(2));
-			assertNotNull(sut.at(3));
-			assertNotNull(sut.at(4));
-			assertNotNull(sut.at(5));
-			assertNotNull(sut.at(6));
-			assertNotNull(sut.at(7));
-			assertNotNull(sut.at(8));
-			assertNotNull(sut.at(9));
+		@Test public void testHasNext1(){
+			assertTrue(sut.hasNext());
+			sut.next();
+			
+			assertTrue(sut.hasNext());
+			sut.next();
+			
+			assertTrue(sut.hasNext());
+			sut.next();
+			
+			assertTrue(sut.hasNext());
+			sut.next();
+			
+			assertTrue(sut.hasNext());
+			sut.next();
+			
+			assertTrue(sut.hasNext());
+			sut.next();
+			
+			assertTrue(sut.hasNext());
+			sut.next();
+			
+			assertTrue(sut.hasNext());
+			sut.next();
+			
+			assertTrue(sut.hasNext());
+			sut.next();
+			
+			assertTrue(sut.hasNext());
+			sut.next();
+			
+			assertFalse(sut.hasNext());
 		}
 		
+		@Test public void testNext1(){
+			assertTrue(sut.hasNext());
+			assertEquals(Integer.valueOf(1), sut.next());
+			
+			assertTrue(sut.hasNext());
+			assertEquals(Integer.valueOf(2), sut.next());
+			
+			assertTrue(sut.hasNext());
+			assertEquals(Integer.valueOf(3), sut.next());
+			
+			assertTrue(sut.hasNext());
+			assertEquals(Integer.valueOf(4), sut.next());
+			
+			assertTrue(sut.hasNext());
+			assertEquals(Integer.valueOf(5), sut.next());
+			
+			assertTrue(sut.hasNext());
+			assertEquals(Integer.valueOf(6), sut.next());
+			
+			assertTrue(sut.hasNext());
+			assertEquals(Integer.valueOf(7), sut.next());
+			
+			assertTrue(sut.hasNext());
+			assertEquals(Integer.valueOf(8), sut.next());
+			
+			assertTrue(sut.hasNext());
+			assertEquals(Integer.valueOf(9), sut.next());
+			
+			assertTrue(sut.hasNext());
+			assertEquals(Integer.valueOf(10), sut.next());
+			
+			assertFalse(sut.hasNext());
+		}
+		
+		@Test public void testSumCollection(){
+			int expectedSum = 55;
+			
+			//This is one way to sum the collection
+			//But it requires that you first get the length then create a for loop to loop over all the enteries
+			//This is longer and you could possibly introduce errors in your loop logic
+			int len = sut.length();
+			int sum1 = 0;
+			
+			for(int i=0; i<len; i++){
+				sum1 = sum1 + sut.at(i);
+			}
+			
+			//The hasNext() and next() functions make it a little neater
+			int sum2 = 0;
+			
+			while(sut.hasNext()){
+				sum2 = sum2 + sut.next();
+			}
+			
+			//Remember while(sut.hasNext()){} is shorthand for while(sut.hasNext() == true){}
+			//You don't have to keep track of any looping variables, you write that logic in one place,
+			//And let the collection manage that itself
+			
+			//Now, both ways should give us 55
+			assertEquals(expectedSum, 	sum1);
+			assertEquals(expectedSum, 	sum2);
+			assertEquals(sum1, 			sum2); //If you get to this line then it will obviously be true.
+		}
+		
+		/*
 		@Test (expected = Exception.class)
 		public void testHasNext2(){
 			assertNotNull(sut2.at(0));
@@ -79,5 +165,5 @@ public class CollectionTests {
 			assertNull(sut.at(10));
 
 		}
-		
+		*/
 }
